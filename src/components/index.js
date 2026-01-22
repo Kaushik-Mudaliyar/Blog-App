604df4ac451b235d66e95792758eb24598d9feee
Kaushik Mudaliyar <kaushikmudaliyar1484@gmail.com>
2026-01-22 16:39:11 +0530

    create Login,Signup and AuthLayout component

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Logo from "./Logo";
-import Container from "./container/Container"
+import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
 
-export {Header,Footer,Logo,Container,LogoutBtn}
+export { Header, Footer, Logo, Container, LogoutBtn, Button, Input };
