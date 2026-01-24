685140fcc6b0f1481f79c50702c8caf4116d507b
Kaushik Mudaliyar <kaushikmudaliyar1484@gmail.com>
2026-01-24 16:49:50 +0530

    create RTE and post form component

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,17 @@ import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
 import Input from "./Input";
+import Select from "./Select";
+import RTE from "./RTE";
 
-export { Header, Footer, Logo, Container, LogoutBtn, Button, Input };
+export {
+  Header,
+  Footer,
+  Logo,
+  Container,
+  LogoutBtn,
+  Button,
+  Input,
+  Select,
+  RTE,
+};
