ef1150d05c74785420a90162dc2a37e51eb2ab9d
Kaushik Mudaliyar <kaushikmudaliyar1484@gmail.com>
2026-02-04 16:03:40 +0530

    Create Post Form and RTE component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,29 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Protected from "./AuthLayout";
+import PostForm from "./post-form/PostForm";
+import Input from "./Input";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import RTE from "./RTE";
+import Select from "./Select";
+import SignUp from "./SignUp";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+  Protected,
+  PostForm,
+  Input,
+  Login,
+  PostCard,
+  RTE,
+  Select,
+  SignUp,
+};
